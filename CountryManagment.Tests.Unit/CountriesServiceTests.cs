c0cf10dfd1895ce3e0e6152c73fa82a80daedf8d
Esmaeil Sheikhi <es.sheikhi@gmail.com>
2024-09-29 11:57:10 +0330

    Start defining CountryService tests based on TDD: 1- AddCountry method tests

--- CountryManagment.Tests.Unit/CountriesServiceTests.cs
+++ CountryManagment.Tests.Unit/CountriesServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,74 @@ namespace CountryManagment.Tests.Unit
         private readonly ICountriesService _countriesService;
         public CountriesServiceTests()
         {
+            _countriesService = new CountriesService();
         }
+
+        [Fact]
+        public void AddCountry_ShouldThrowException_WhenCountryIsNull()
+        {
+            //Arrange
+            CountryAddRequest countryAddRequest = null;
+
+            //Act
+            var result = () => _countriesService.AddCountry(countryAddRequest);
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(result);
+        }
+
+        [Fact]
+        public void AddCountry_ShouldThrowException_WhenCountryNameIsNull()
+        {
+            //Arrange
+            CountryAddRequest countryAddRequest = new()
+            {
+                CountryName = null
+            };
+
+            //Act
+            var result = () => _countriesService.AddCountry(countryAddRequest);
+
+            //Assert
+            Assert.Throws<ArgumentException>(result);
+        }
+
+        [Fact]
+        public void AddCountry_ShouldThrowException_WhenCountryNameIsDuplicated()
+        {
+            //Arrange
+            CountryAddRequest countryAddRequest1 = new()
+            {
+                CountryName = "UK"
+            };
+            CountryAddRequest countryAddRequest2 = new()
+            {
+                CountryName = "USA"
+            };
+
+            //Act
+            _countriesService.AddCountry(countryAddRequest1);
+            var result = () => _countriesService.AddCountry(countryAddRequest2);
+
+            //Assert
+            Assert.Throws<ArgumentException>(result);
+        }
+
+        [Fact]
+        public void AddCountry_ShouldReturnProperCountry_WhenEverythingIsOK()
+        {
+            //Arrange
+            CountryAddRequest countryAddRequest = new()
+            {
+                CountryName = "Germany"
+            };
+
+            //Act
+            var result = _countriesService.AddCountry(countryAddRequest);
+
+            //Assert
+            Assert.True(result?.CountryId != null);
+        }
+
     }
 }
