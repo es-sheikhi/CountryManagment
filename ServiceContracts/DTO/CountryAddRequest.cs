a0af07330d05d33ec4a5ecc375edeeb7eb3bb6ca
Esmaeil Sheikhi <es.sheikhi@gmail.com>
2024-09-29 11:04:54 +0330

    Adding Country DTOs to ServiceContracts project

---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    public class CountryAddRequest
+    {
+        public string? CountryName { get; set; }
+
+        public Country ToCountry() => new Country { CountryName = CountryName };
+    }
+}
---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class is used as return type for most of CountriesService methods
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
